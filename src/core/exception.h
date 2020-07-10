44b11aa5c0efd4cafaf2d7527c7ecafe187f3bd7
neckutrek <marcus.adam.johansson@gmail.com>
2020-07-10 17:05:43 +0200

    Add tg::Exception class

---
+++ src/core/exception.h
@@ -0,0 +1,13 @@
+#ifndef __THATGAME_CORE_EXCEPTION_INCGRD__
+#define __THATGAME_CORE_EXCEPTION_INCGRD__
+
+#include <stdexcept>
+
+namespace tg
+{
+
+using Exception = std::runtime_error;
+
+} // namespace tg
+
+#endif
